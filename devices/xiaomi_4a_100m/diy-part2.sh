e8320c0b2a9ff80f68f2525e481556b2ec439d06
“wzwttgit config --global user.name “wzwtt <wzwttol@outlook.com>
2022-02-26 16:06:51 +0800

    remove xiaomi-4a-100m uboot read-only

--- devices/xiaomi_4a_100m/diy-part2.sh
+++ devices/xiaomi_4a_100m/diy-part2.sh
@@ -12,6 +12,7 @@
 
 
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+sed -i '/read-only;/d' target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4a-100m.dts
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
