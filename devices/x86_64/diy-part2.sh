49908d46cefe1e315c7df22901c927332837e1f3
George Wang <wzwttol@outlook.com>
2024-07-25 22:32:29 +0800

    Update diy-part2.sh

--- devices/x86_64/diy-part2.sh
+++ devices/x86_64/diy-part2.sh
@@ -12,7 +12,7 @@
 
 git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
